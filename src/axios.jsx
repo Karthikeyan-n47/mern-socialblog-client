bbe77c9b4358a369eca2ea242d56f0418dd0dba3
Karthikeyan-n47 <karthikeyan.n47@gmail.com>
2024-06-21 20:45:28 +0530

    added api url

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://mern-socialblog-api.vercel.app/api",
   withCredentials: true,
 });
 
